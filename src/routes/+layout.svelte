e9ab2ab4cbfd5688ce9813ed3ad325b8274cd192
GabrielBarbosaGV <gabrielbgasparv@gmail.com>
2023-01-28 21:18:11 +0000

    Initial site content
    The site is intended to be a blog, and so, the first few introductory
    words have been written.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import "../app.css";
+</script>
+
+<div class="h-12 bg-black flex flex-col justify-center">
+    <span class="text-slate-100 text-2xl font-bold p-3">
+        GBGV
+    </span>
+</div>
+
+<div class="text-yellow-100 flex flex-col align-middle p-4">
+    <slot />
+</div>
+
+<style>
+    :global(body) {
+        background-color: #1d1d1d;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<span class="text-3xl text-center">
+    Hello there!
+</span>
+
+<p class="p-4">
+    I've been getting into the habit of writing down my thoughts, for better control over life aspects. I thought that it might also be a good idea
+    to write down these thoughts for others to see, and to get feedback as well. I hope you like the content :)
+</p>
