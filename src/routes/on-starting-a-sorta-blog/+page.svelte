9efdbe92eee1d6d06a8dd826cf707b8819b00e64
GabrielBarbosaGV <gabrielbgasparv@gmail.com>
2023-02-01 21:28:37 +0000

    First post

---
+++ src/routes/on-starting-a-sorta-blog/+page.svelte
@@ -0,0 +1,55 @@
+<div class="fold:mx-10 xl:mx-40 mx-2 text-justify">
+    Hello to all, as you might have guessed, my name is Gabriel Barbosa. I've decided
+    to start writing a sorta blog as to share my thoughts and actually get feedback on
+    them. Of course, since this site is static, I cannot actually add comments (maybe
+    when I'm no longer feeling lazy), so I expect you to send me good ol' e-mails. If
+    you're feeling like it, just click on the "GBGV" on the top left of the page. But
+    let's get to the point - what's in it for you to read this at all?
+
+    <div class="text-[25px] text-center my-6">
+        GETTING TO KNOW YOUR SOON-TO-BE FAVORITE RAMBLER
+    </div>
+
+    Well, my friend, say you make efforts towards being a clean coder, hate poorly
+    written tests, wish you could use the tools you know are best (SvelteKit and 
+    tailwind-css) at your job's jQueried spaghetti of a SEO hell website, and are
+    kind of bummed that you just won't stop dying in your Tarkov raids - then
+    you'll feel right at home here.
+
+    <br />
+    <br />
+
+    To be real, I've no idea of what I'm doing. I just wanted to create a website
+    using my favourite technologies, and wanted to use GitHub pages as well. If
+    you somehow landed here, and end up enjoying a read, hell, so be it. I intend
+    on writing on just about anything from philosophy to good food to programming,
+    but especially programming, as that's what feeds me and I enjoy doing most.
+
+    <br />
+    <br />
+
+    I don't know about you, but I was that guy that'd write functions with hundreds
+    of LOC, true awful monstrosities of tangled functionality, and would dread the
+    choice of career that made the simplest of change requests into a quest of
+    faith and spite to unfrick the plopped mess of a product. It went on for a
+    while, and I knew there ought to be better ways to write code. I loosely and
+    gradually started adopting these choices through my own painful experiences,
+    till a friend convinced me to finally give Clean Code a go. Twas a religious
+    experience, I must say, and using the principles, to this day, makes life a tad
+    easier every time you clean the playground of that legacy codebase.
+
+    <br />
+    <br />
+
+    While I've got Clean Architecture pending, I can only thank Robert C. Martin
+    for clarifying so much on how to be clean. If you haven't read that book yet,
+    get on it, you won't regret it.
+
+
+    <br />
+    <br />
+
+    I'm not sure whether this'll be the first or last place people who read my posts
+    end up, so let me know - and keep your hopes high. I know mine are, so here's to
+    great things in the future. Cya! :D
+</div>
