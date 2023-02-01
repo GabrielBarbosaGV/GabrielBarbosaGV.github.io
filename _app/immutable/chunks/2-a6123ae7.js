4ae5280c33d6e148ea3d6d934199dd524635d1d7
GabrielBarbosaGV <gabrielbgasparv@gmail.com>
2023-02-01 23:27:56 +0000

    Adding first post

---
+++ _app/immutable/chunks/2-a6123ae7.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-7d160b4c.js";export{t as component};
---
+++ _app/immutable/chunks/3-baa416cd.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/on-starting-a-sorta-blog/_page.svelte-d896eac4.js";export{t as component};
